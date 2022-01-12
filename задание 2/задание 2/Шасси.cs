facda520290dccc3d9b24221c7c9ade1900cfbb3
nikichna03 <94111196+nikichna03@users.noreply.github.com>
2022-01-13 01:18:58 +0300

    Add files via upload

---
+++ задание 2/задание 2/Шасси.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_2
+{
+    class Шасси : Трансмиссия
+    {
+        public int wheel { get; set; }
+        public int numer { get; set; }
+        public double load { get; set; }
+
+        public Шасси(int wheel, int numer, double load, int broadcast, string typ, string manufacturer, double power, double size, bool type, string id) : base(broadcast, typ, manufacturer, power, size, type, id)
+        {
+            this.wheel = wheel;
+            this.numer = numer;
+            this.load = load;
+        }
+
+        public Шасси() { }
+
+        public void Text()
+        {
+            Console.WriteLine("Двигатель:\n" + power + " " + size + " " + type + " " + id);
+            Console.WriteLine();
+            Console.WriteLine("Трансмиссия:\n" + broadcast + " " + typ + " " + manufacturer);
+            Console.WriteLine();
+            Console.WriteLine("Шасси:\n" + wheel + " " + numer + " " + load);
+            Console.WriteLine();
+        }
+    }
+}
