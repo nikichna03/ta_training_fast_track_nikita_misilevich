320bee46d2faf3227e4f06dc56a9321b4af6c542
nikichna <94111196+nikichna03@users.noreply.github.com>
2022-01-17 22:16:34 +0300

    Add files via upload

---
+++ задание 3/IFlyable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_3
+{
+    interface IFlyable
+    {
+        public void FlyTo(Coordinate null_dot)//опредедляет растояние
+        {
+        }
+        public void GetFlyTime(Coordinate null_dot)//определяет время в ч
+        {               
+        }
+    }
+}
---
+++ задание 4/Engine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_4
+{
+    class Engine
+    {
+        public double power { get; set; }
+        public double size { get; set; }
+        public bool type { get; set; }
+        public string id { get; set; }
+
+        public Engine(double power, double size, bool type, string id)
+        {
+            this.power = power;
+            this.size = size;
+            this.type = type;
+            this.id = id;
+        }
+
+        public Engine() { }
+    }
+}
